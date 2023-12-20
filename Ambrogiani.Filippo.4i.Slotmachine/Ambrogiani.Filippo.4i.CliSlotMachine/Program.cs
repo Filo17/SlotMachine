632e8f0d374bc3ff3367789eb1ed6a0698caa494
Filippo Ambrogiani <81953792+Filo17@users.noreply.github.com>
2023-12-20 09:55:46 +0100

    Add files via upload

---
+++ Ambrogiani.Filippo.4i.Slotmachine/Ambrogiani.Filippo.4i.CliSlotMachine/Program.cs
@@ -0,0 +1,13 @@
+﻿using Ambrogiani.Filippo._4i.LibSlotMachine;
+
+namespace Ambrogiani.Filippo._4i.CliSlotMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, SlotMachine!");
+            SlotMachine s = new SlotMachine(10);
+        }
+    }
+}
